a7ee5be473132024a9da1925cf1b6966a5de6c1a
Matheus Ramos <92637216+matheuseduardoramos@users.noreply.github.com>
2022-06-10 16:02:50 -0300

    Add files via upload

---
+++ Lista 5/01.c
@@ -0,0 +1,1 @@
+Pilhas são estruturas de dados que seguem a ordem “LIFO” (Last in first out), ou seja, o último termo adicionado a pilha será o primeiro a sair.
---
+++ Lista 5/02.c
@@ -0,0 +1,1 @@
+Funções recursivas, janelas em um programa.
